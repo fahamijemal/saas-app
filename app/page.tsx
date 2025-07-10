cd10369ea4669ba24e58a8e24d5106733c0fbaf3
Fahami Jemal <fahamijemal1@gmail.com>
2025-07-10 18:24:39 +0300

    feat: add CompanionCard and CompanionsList components with responsive UI and subject-based styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,43 @@
+'use client';
+import ComponentCard from "@/components/ComponentCard";
+import CTA from "@/components/CTA";
+import CompanionsList from "@/components/CompanionsList";
+
 export default function HomePage() {
     return (
-        <div>
-            <h2>Hello World Page</h2>
-        </div>
+        <main>
+            <h1>Popular companions</h1>
+            <section className="home-section">
+                <ComponentCard
+                    id="123"
+                    name="Neura the Explorer"
+                    topic="Neural Network of the brain"
+                    subject="Science"
+                    duration={45}
+                    color="#ffda6e"
+                />
+                <ComponentCard
+                    id="124"
+                    name="Neura the Explorer"
+                    topic="Neural Network of the brain"
+                    subject="Maths"
+                    duration={45}
+                    color="#e5dfff"
+                />
+                <ComponentCard
+                    id="125"
+                    name="Neura the Explorer"
+                    topic="Language"
+                    subject="Science"
+                    duration={30}
+                    color="#faca6e"
+                />
+            </section>
+
+            <section className="home-section">
+                <CTA />
+                <CompanionsList />
+            </section>
+        </main>
     );
 }
