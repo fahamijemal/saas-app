43637d2308bca821d8c5a4731d680d8cc3434830
Fahami Jemal <fahamijemal1@gmail.com>
2025-07-11 16:41:46 +0300

    update Navbar component with Clerk SignInButton and UserButton integration

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,26 +2,36 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import NavItems from "@/components/NavItems";
-
+import { SignInButton, SignedOut, SignedIn, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
-  return (
-    <nav className='navbar'>
-      <Link href="/">
-        <div className="flex items-center gap-2 cursor-pointer">
-          <Image src="/images/logo.svg" 
-           alt="logo"  
-           width={46} 
-           height={44}
-           />
-        </div>
-      </Link>
-      <div className='flex items-center gap-8'>
-          <NavItems/>
-          <p>Sign In</p>
-      </div>
-    </nav>
-  )
-}
+    return (
+        <nav className='navbar'>
+            <Link href="/">
+                <div className="flex items-center gap-2 cursor-pointer">
+                    <Image src="/images/logo.svg" alt="logo" width={46} height={44} />
+                </div>
+            </Link>
 
-export default Navbar
+            <div className='flex items-center gap-8'>
+                <NavItems />
+
+                {/* When the user is signed out, show Sign In button */}
+                <SignedOut>
+                    <div className='flex items-center gap-2'>
+                        <SignInButton>
+                            <button className='btn-primary'>Sign In</button>
+                        </SignInButton>
+                    </div>
+                </SignedOut>
+
+                {/* When the user is signed in, show UserButton */}
+                <SignedIn>
+                    <UserButton afterSignOutUrl="/" />
+                </SignedIn>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
