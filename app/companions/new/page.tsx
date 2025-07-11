f407aaf4efdce1ed493355a594588bb54f98d6d2
Fahami Jemal <fahamijemal1@gmail.com>
2025-07-11 16:29:04 +0300

    Secure new companion page with Clerk auth check

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,11 +1,17 @@
 
 import CompanionForm from "@/components/CompanionForm";
+import {auth} from "@clerk/nextjs/server";
+import {redirect} from "next/navigation";
 
-export default function newCompanions() {
+export default async function newCompanion (){
+    const {userId} = await auth();
+    if (!userId) {
+        redirect('/sign-in')
+    }
     return (
         <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
             <article className="w-full gap-4 flex flex-col">
-                <CompanionForm />
+                <CompanionForm/>
             </article>
         </main>
     );
