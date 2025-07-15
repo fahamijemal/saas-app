30caf1c9d8301b68b04dce7810d322b8d322fc1c
Fahami Jemal <fahamijemal1@gmail.com>
2025-07-15 19:19:43 +0300

    fixed some error

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,27 +1,30 @@
-"use client";
-
-import { cn } from "@/lib/utils";
+'use client';
 import Link from "next/link";
 import {usePathname} from "next/navigation";
+import {cn} from "@/lib/utils";
 
 const navItems = [
-    {label: 'Home', href: '/'},
-    {label: 'Learning Companions', href: '/companions'},
-    {label: 'My-journey', href: '/my-journey'},
-
+    { label:'Home', href: '/' },
+    { label: 'Learning Companions', href: '/companions' },
+    { label: 'My Journey', href: 'my-journey' },
 ]
+
 const NavItems = () => {
-    const pathname =usePathname();
-    return(
-        <nav className='flex items-center gap-4'>
-            {navItems.map(({label, href})=>(
+    const pathname = usePathname();
+
+    return (
+        <nav className="flex items-center gap-4">
+            {navItems.map(({ label, href }) => (
                 <Link
                     href={href}
                     key={label}
-                    className={cn(pathname===href && 'text-primary font-semibold')}
-                >{label}</Link>
+                    className={cn(pathname === href && 'text-primary font-semibold')}
+                >
+                    {label}
+                </Link>
             ))}
         </nav>
     )
 }
-export default NavItems;
+
+export default NavItems
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,37 +1,34 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import NavItems from "@/components/NavItems";
-import { SignInButton, SignedOut, SignedIn, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
     return (
-        <nav className='navbar'>
+        <nav className="navbar">
             <Link href="/">
-                <div className="flex items-center gap-2 cursor-pointer">
-                    <Image src="/images/logo.svg" alt="logo" width={46} height={44} />
+                <div className="flex items-center gap-2.5 cursor-pointer">
+                    <Image
+                        src="/images/logo.svg"
+                        alt="logo"
+                        width={46}
+                        height={44}
+                    />
                 </div>
             </Link>
-
-            <div className='flex items-center gap-8'>
+            <div className="flex items-center gap-8">
                 <NavItems />
-
-                {/* When the user is signed out, show Sign In button */}
                 <SignedOut>
-                    <div className='flex items-center gap-2'>
-                        <SignInButton>
-                            <button className='btn-primary'>Sign In</button>
-                        </SignInButton>
-                    </div>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
                 </SignedOut>
-
-                {/* When the user is signed in, show UserButton */}
                 <SignedIn>
-                    <UserButton afterSignOutUrl="/" />
+                    <UserButton />
                 </SignedIn>
             </div>
         </nav>
-    );
-};
+    )
+}
 
-export default Navbar;
+export default Navbar
