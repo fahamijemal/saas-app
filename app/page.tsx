c525513a7c74cb987d4fbfd30ea5e9e94134ed38
Fahami Jemal <fahamijemal1@gmail.com>
2025-07-16 09:18:47 +0300

    update home page displaying popular companions and recent sessions with CompanionCard and CompanionsList components

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,39 @@
-'use client';
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 import {recentSessions} from "@/constants";
-import Cta from "@/components/CTA";
+import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import {getSubjectColor} from "@/lib/utils";
 
-export default function HomePage() {
-    return (
-        <main>
-            <h1>Popular companions</h1>
-            <section className="home-section">
-                <CompanionCard
-                    id="123"
-                    name="Neura the Explorer"
-                    topic="Neural Network of the brain"
-                    subject="Science"
-                    duration={45}
-                    color="#ffda6e"
-                />
-                <CompanionCard
-                    id="124"
-                    name="Neura the Explorer"
-                    topic="Neural Network of the brain"
-                    subject="Maths"
-                    duration={45}
-                    color="#e5dfff"
-                />
-                <CompanionCard
-                    id="125"
-                    name="Neura the Explorer"
-                    topic="Language"
-                    subject="Science"
-                    duration={30}
-                    color="#faca6e"
-                />
-            </section>
+const Page = async () => {
+    const companions = await getAllCompanions({ limit: 3 });
+    const recentSessionsCompanions = await getRecentSessions(10);
 
-            <section className="home-section">
-                <Cta />
-                <CompanionsList
-                    title="Recently completed sessions"
-                    companions={recentSessions}
-                    className='w-2/3 max-lg:w-full'
+  return (
+    <main>
+      <h1>Popular Companions</h1>
+
+        <section className="home-section">
+            {companions.map((companion) => (
+                <CompanionCard
+                    key={companion.id}
+                    {...companion}
+                    color={getSubjectColor(companion.subject)}
                 />
-            </section>
-        </main>
-    );
+            ))}
+
+        </section>
+
+        <section className="home-section">
+            <CompanionsList
+                title="Recently completed sessions"
+                companions={recentSessionsCompanions}
+                classNames="w-2/3 max-lg:w-full"
+            />
+            <CTA />
+        </section>
+    </main>
+  )
 }
+
+export default Page
