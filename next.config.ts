b2eea956280fd9c0334a6823fcb8cecfa64649cb
Fahami Jemal <fahamijemal1@gmail.com>
2025-07-16 09:31:20 +0300

    chore: update Sentry config and package files; add API routes and global error handler

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        domains: ["img.clerk.com"],
+    },
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+    org: "fahami-jemal",
+    project: "javascript-nextjs",
+    silent: !process.env.CI,
+    widenClientFileUpload: true,
+    tunnelRoute: "/monitoring",
+    disableLogger: true,
+    automaticVercelMonitors: true,
+});
